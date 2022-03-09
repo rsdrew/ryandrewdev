47df109cbd900c96a3e4780516cbc69af662f576
Ryan Drew <rsdrew@umich.edu>
2022-03-08 23:58:04 -0500

    Added projects. Still a WIP to clean up.

--- _site/assets/js/styling.js
+++ _site/assets/js/styling.js
@@ -1,18 +1,42 @@
 $(document).ready(function() {
     showText();
+    projectImageOnHover();
 });
 
 function showText() {
     setTimeout(function () {
-        $("#h1HiImRyan").removeClass('invisible');
+        $("#h1HiImRyan").removeClass('hide');
     }, 500);
 
     setTimeout(function () {
-        $("#h2ShortDescription").removeClass('invisible');
+        $("#h2ShortDescription").removeClass('hide');
     }, 1000);
 
     setTimeout(function () {
-        $("#h3ScrollDown").removeClass('invisible');
-        $("#imgDownArrow").removeClass('invisible');
+        $("#h3ScrollDown").removeClass('hide');
+        $("#imgDownArrow").removeClass('hide');
     }, 2000)
+}
+
+function projectImageOnHover() {
+    var projectImgAndButtons = $(".projectImgAndButton");
+    var overlays = $(".overlay");
+    var btnViewProjects = $(".btnViewProject");
+
+    for(let i=0; i < projectImgAndButtons.length; i++) {
+        let projectImgAndButton = projectImgAndButtons.eq(i);
+        let overlay = overlays.eq(i);
+        let btnViewProject = btnViewProjects.eq(i);
+
+        projectImgAndButton.hover(
+            () => { // hover
+                overlay.removeClass('hide');
+                btnViewProject.removeClass('hide');
+            },
+            () => { // out
+                overlay.addClass('hide');
+                btnViewProject.addClass('hide');
+            }
+        )
+    }
 }
