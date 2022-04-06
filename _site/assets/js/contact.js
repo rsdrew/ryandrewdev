044fbb4562bbb9abf889ef4fd70be4cb8f34cb1a
Ryan Drew <rsdrew@umich.edu>
2022-04-05 23:28:58 -0400

    Added contact form validation

--- _site/assets/js/contact.js
+++ _site/assets/js/contact.js
@@ -1,10 +1,62 @@
 $(document).ready(function () {
-    $('#btnSend').click(clearForm);
-})
+    $('#btnSend').click(btnSend_Click);
+    $('#txtName').on('input', function() {formControlChange('Name')});
+    $('#txtEmail').on('input', function() {formControlChange('Email')});
+    $('#txtMessage').on('input', function() {formControlChange('Message')});
+});
+
+function formControlChange(controlName) {
+    $(`#rfv${controlName}`).addClass('hidden');
+}
+
+function hideErrorMessages() {
+    $('#rfvName').addClass('hidden');
+    $('#rfvEmail').addClass('hidden');
+    $('#rfvMessage').addClass('hidden');
+}
+
+function validateEmail(emailAdress) {
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailAdress.match(regexEmail)) {
+    return true; 
+  } else {
+    return false; 
+  }
+}
+
+function validateContactForm(event) {
+    let valid = true;
+    if ($('#txtName').val() === "") {
+        $('#rfvName').removeClass('hidden');
+        valid = false;
+    }
+
+    if (!validateEmail($('#txtEmail').val())) {
+        $('#rfvEmail').removeClass('hidden');
+        valid = false;
+    }
+
+    if ($('#txtMessage').val() === "") {
+        $('#rfvMessage').removeClass('hidden');
+        valid = false;
+    }
+
+    if (!valid) {
+        event.preventDefault();
+    }
+}
 
 function clearForm() {
     $('#txtName').val('');
     $('#txtEmail').val('');
     $('#txtMessage').val('');
     $('#successMessage').removeClass('hidden');
+}
+
+function btnSend_Click(event) {
+    hideErrorMessages();
+    let valid = validateContactForm(event);
+    if (valid) {
+        clearContactForm();
+    }
 }
